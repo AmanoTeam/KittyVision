eec9e6e973bb5a7757185606bfb6842840050eea
Alisson Lauffer <alissonvitortc@gmail.com>
2023-04-01 17:12:47 -0300

    implement mutation detector

--- content.js
+++ content.js
@@ -1,10 +1,28 @@
 const catUrl = "https://cataas.com/cat";
 
-function replaceImages() {
-  const images = document.getElementsByTagName("img");
+function replaceImages(images) {
   for (let i = 0; i < images.length; i++) {
-    images[i].src = `${catUrl}?${i}`;
+    if (!images[i].src.startsWith(catUrl)) {
+      images[i].src = `${catUrl}?${images[i].src}`;
+    }
   }
 }
 
-replaceImages();
+replaceImages(document.getElementsByTagName("img"));
+
+const observer = new MutationObserver((mutationsList, _observer) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      mutation.addedNodes.forEach((addedNode) => {
+        let elements = addedNode.getElementsByTagName("img");
+        replaceImages(elements);
+      })
+    } else if (mutation.type === "attributes" && mutation.target.tagName.toLowerCase() === "img") {
+      if (!mutation.target.src.startsWith(catUrl)) {
+        mutation.target.src = `${catUrl}?${mutation.target.src}`;
+      }
+    }
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true, attributes: true, attributeFilter: ["src"] });
